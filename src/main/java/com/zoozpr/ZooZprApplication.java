576bc44f6c1f67668752b64a7d466d90b2877feb
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-09-15 18:46:14 +0200

    Initial commit.

---
+++ src/main/java/com/zoozpr/ZooZprApplication.java
@@ -0,0 +1,13 @@
+package com.zoozpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooZprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooZprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoozpr/ZooZprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoozpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooZprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
